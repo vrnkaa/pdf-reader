c27a7a1e6d8c8a22a67304d3aecc89cc5f5111ff
aka <aka@aka.com>
2022-01-31 19:06:40 +0300

    Initial commit

---
+++ src/main/java/com/vrnkaa/pdfreader/PdfReaderApplication.java
@@ -0,0 +1,13 @@
+package com.vrnkaa.pdfreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vrnkaa/pdfreader/PdfReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrnkaa.pdfreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
